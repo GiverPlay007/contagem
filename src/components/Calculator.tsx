025eb3e0b538ebec2c0818832770237f74ad1033
GiverPlay007 <play.giver.007@gmail.com>
2022-04-17 13:08:08 -0300

    Added float support and changes calculator formula

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -9,7 +9,9 @@ export function Calculator() {
     const totalWeight = form.totalWeight.value;
     const tare = form.tare.value;
 
-    const result = (totalWeight - tare) * 10 / groupWeight
+    const individualWeight = groupWeight / 10;
+    const result = (totalWeight - tare) / individualWeight;
+
     form.result.value = result.toFixed(0);
   };
 
@@ -18,17 +20,17 @@ export function Calculator() {
       <form onSubmit={calculate}>
         <div className={styles.inputBlock}>
           <label htmlFor="groupWeight">Peso de 10 itens</label>
-          <input id="groupWeight" type="number" defaultValue={0} min={0} required/>
+          <input id="groupWeight" type="number" step="any" defaultValue={0} min={0} required/>
         </div>
 
         <div className={styles.inputBlock}>
           <label htmlFor="totalWeight">Peso total</label>
-          <input id="totalWeight" type="number" defaultValue={0} min={0} required/>
+          <input id="totalWeight" type="number" step="any" defaultValue={0} min={0} required/>
         </div>
 
         <div className={styles.inputBlock}>
           <label htmlFor="tare">Tara</label>
-          <input id="tare" type="number" defaultValue={0} min={0} required/>
+          <input id="tare" type="number" step="any" defaultValue={0} min={0} required/>
         </div>
 
         <div className={styles.inputBlock}>
